f7edaa1ad23a6af49b71372a59b91e7409bccab8
atcz <86995996+atcz@users.noreply.github.com>
2022-05-30 08:55:38 -0700

    New DCS Bios release version

--- src/first_setup.py
+++ src/first_setup.py
@@ -8,7 +8,7 @@ import tempfile
 import requests
 import zipfile
 
-DCS_BIOS_VERSION = '0.7.44'
+DCS_BIOS_VERSION = '0.7.45'
 DCS_BIOS_URL = "https://github.com/DCSFlightpanels/dcs-bios/releases/download/v{}/DCS-BIOS_{}.zip"
 
 logger = get_logger(__name__)
